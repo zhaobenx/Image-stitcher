c7419d5fc8b40425bac7314b9b2170954f4bfc1b
zhaobenx <ghostbenzhao@gmail.com>
2018-04-17 22:36:51 +0800

    Sround with try catch

--- py/batch_stich.py
+++ py/batch_stich.py
@@ -16,25 +16,28 @@ def main():
     # main()
     os.chdir(os.path.dirname(__file__))
 
-    number = 17
+    number = 19
     file1 = "../resource/{}-right*.jpg".format(number)
     file2 = "../resource/{}-left.jpg".format(number)
 
     start_time = time.time()
-    for method in (Method.SIFT, Method.ORB):
+    try:
+        for method in (Method.ORB, Method.SIFT):
 
-        for f in glob.glob(file1):
-            print(f)
-            name = f.replace('right', method.name)
-            # print(file2, name)
+            for f in glob.glob(file1):
+                print(f, method)
+                name = f.replace('right', method.name)
+                # print(file2, name)
 
-            img2 = cv2.imread(file2)
-            img1 = cv2.imread(f)
-            sticher = Sticher(img1, img2, method=method)
-            sticher.stich(show_result=False)
-            cv2.imwrite(name, sticher.image)
-            print("Time: ", time.time() - start_time)
-            # print("M: ", sticher.M)
+                img2 = cv2.imread(file2)
+                img1 = cv2.imread(f)
+                sticher = Sticher(img1, img2, method=method)
+                sticher.stich(show_result=False)
+                cv2.imwrite(name, sticher.image)
+                print("Time: ", time.time() - start_time)
+                # print("M: ", sticher.M)
+    except Exception as e:
+        print("Error!: ", e)
     print('\a')
 
 
