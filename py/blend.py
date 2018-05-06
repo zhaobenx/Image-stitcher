6aad7f15a6bbbcbdd5c6b3b49ab23ab32fe2bcb3
zhaobenx <ghostbenzhao@gmail.com>
2018-05-06 12:36:18 +0800

    :tea: Refactor average blend
    将平均算法移到blend中

--- py/blend.py
+++ py/blend.py
@@ -13,13 +13,18 @@ from scipy.ndimage.filters import gaussian_filter
 import cv2
 
 
+def show_image(image: np.ndarray) -> None:
+    from PIL import Image
+    Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).show()
+
+
 class Blend:
     pass
 
 
 class GaussianBlend(Blend):
 
-    LEVEL = 6
+    LEVEL = 3
 
     def __init__(self, image1: np.ndarray, image2: np.ndarray, mask: np.ndarray):
         self.image1 = image1
@@ -84,24 +89,66 @@ class GaussianBlend(Blend):
         return result  # .astype(np.uint8)
 
 
-def gaussian_blend(image1: np.ndarray, image2: np.ndarray, mask: np.ndarray):
+def average_blend(image1: np.ndarray, image2: np.ndarray) -> np.ndarray:
+    """平均算法拼合
+
+    Args:
+        image1 (np.ndarray): 图片一
+        image2 (np.ndarray): 图片二
+
+    Returns:
+        np.ndarray: 拼合后图像
+    """
+
+    assert(image1.shape == image2.shape)
+    result = np.zeros(image1.shape, dtype='uint8')
+
+    # image1 != 0 && image2 !=0:
+    overlap = np.logical_and(
+        np.all(np.not_equal(image1, [0, 0, 0]), axis=2),
+        np.all(np.not_equal(image2, [0, 0, 0]), axis=2),
+    )
+    # 重叠处用平均值
+    result[overlap] = np.average(
+        np.array([image1[overlap], image2[overlap]]), axis=0
+    ) .astype(np.uint8)
+    # 非重叠处采选最大值
+    not_overlap = np.logical_not(overlap)
+    result[not_overlap] = np.maximum(
+        image1[not_overlap], image2[not_overlap])
+
+    return result
+
+
+def gaussian_blend(image1: np.ndarray, image2: np.ndarray, mask: np.ndarray, mask_blend=5):
+    mask = gaussian_filter(mask.astype(float), (mask_blend, mask_blend, 0))
+    show_image((mask * 255).astype('uint8'))
+    show_image(image1)
+    show_image(image2)
+
     return GaussianBlend(image1, image2, mask).blend()
+
+
+def direct_blend(image1: np.ndarray, image2: np.ndarray, mask: np.ndarray, mask_blend=0):
+    mask = gaussian_filter(mask.astype(float), (mask_blend, mask_blend, 0))
+    if np.issubdtype(mask.dtype, np.integer):
+        mask = mask / 255
+    show_image((mask * 255).astype('uint8'))
+    show_image(image1)
+    show_image(image2)
+
+    return (image1 * mask + image2 * (1 - mask)).astype('uint8')
 
 
 def test():
     os.chdir(os.path.dirname(__file__))
 
-    def show_image(image: np.ndarray) -> None:
-        from PIL import Image
-        Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).show()
-
-    image1 = cv2.imread("../resource/1-left.jpg")
-    image2 = cv2.imread("../resource/1-right.jpg")
+    image1 = cv2.imread("../example/3-left.jpg")
+    image2 = cv2.imread("../example/3-right.jpg")
     show_image(np.concatenate((image1, image2), axis=0))
-    mask = np.zeros(image1.shape, 'uint8')
-    mask[:600] = 255
-    mask = gaussian_filter(mask, (5,5,0))
-    show_image(mask)
+    mask = np.zeros(image1.shape)
+    mask[:600] = 1.0
+    mask = gaussian_filter(mask, (5, 5, 0))
     show_image(gaussian_blend(image1, image2, mask))
 
 
